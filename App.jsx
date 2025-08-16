5ad634e345dedf809782d5903b7d5d583dc95309
sid2428 <kartikgaikwad2828@gmail.com>
2025-08-16 16:05:03 +0530

    C1

--- App.jsx
+++ App.jsx
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
 import React from 'react';
 import "./App.css"
 import ToDoList from "./todolist.jsx";
 
-=======
-import CommentBox from "./CommentBox";
-import "./App.css"
-import Counter from "./Counter.jsx"
-import ToDoList from "./todolist.jsx";
-import Tbr from "./tbr.jsx";
-import LudoBoard from "./LudoBoard.jsx";
->>>>>>> 543d8f15f850cccab62049b9bb79819fc8567fbf
-
 function App() {
   return (
     <div>
-  
       <ToDoList/>
-
     </div>
   );
 }
